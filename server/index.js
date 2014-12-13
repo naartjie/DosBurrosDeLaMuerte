5b809b8734dbb15d536c3101df49f7d6c2c181c8
Marcin Jekot <marcin@jekot.net>
2014-12-13 13:21:11 +0200

    pretty print JSON and externalise static data

--- server/index.js
+++ server/index.js
@@ -2,44 +2,16 @@
 
 var express = require('express');
 var app = express();
+var data = require('./lib/data');
 
+
+var pretty = function(data) {
+    return JSON.stringify(data, null, 2);
+}
 
 app.get('/api', function(req, res) {
-    res.json({
-        days: [
-            {
-                name: 'Today',
-                chunks: [{
-                    hour: '7am',
-                    temp: 24,
-                    precipitation: 0.8,
-                    cloudcover: [0.8, 0.6, 0],
-                    wind: {
-                        speed: 5,
-                        gusts: 12,
-                        direction: {
-                            degrees: 215,
-                            cardinal: 'NW',
-                        }
-                    }
-                },
-                {
-                    hour: '10am',
-                    temp: 26,
-                    precipitation: 0.5,
-                    cloudcover: [0.4, 0.2, 0],
-                    wind: {
-                        speed: 10,
-                        gusts: 18,
-                        direction: {
-                            degrees: 215,
-                            cardinal: 'NW',
-                        }
-                    }
-                }]
-            }
-        ]
-    });
+    res.set('Content-Type', 'application/json');
+    res.send(pretty(data));
 });
 
 var port = process.env.PORT || 3000;
