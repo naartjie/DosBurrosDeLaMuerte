d0a0bd66ce6bc7d8d7b0b0c0cd7661f2a86f520c
Marcin Jekot <marcin@jekot.net>
2014-12-13 21:42:56 +0200

    oops forgot this guy

---
+++ scraper/scraper.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var durbanUrl = 'www.windguru.cz/int/index.php?sc=4865';
+
+// var http = require('q-io/http');
+
+module.exports = function pullDataz() {
+
+    // return http.get(durbanUrl);
+
+};
